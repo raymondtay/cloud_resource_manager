e035c66f82facaa354866653fa7d997938021160
Raymond <raymondtay1974@gmail.com>
2011-10-28 17:21:03 +0800

    compilation script & execution script

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ./lib/mongo-2.6.5.jar:./lib/xmlunit-1.3.jar:./lib/org.restlet.jar:./lib/org.restlet.ext.xml.jar:./lib/ST-4.0.4.jar:./lib/antlr-3.3-complete.jar:. ResourceManager
